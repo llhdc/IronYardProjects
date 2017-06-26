5743b14541f070007d9b130885c7fdd4fc7c5905
Leonard <leonard.horne@gmail.com>
2017-06-26 14:48:13 -0400

    Add practice files to TIY projects directory.

---
+++ script.bash
@@ -0,0 +1,4 @@
+#1/usr/bin/env bash
+
+ls
+echo hi > hi-file
---
+++ script2.bash
@@ -0,0 +1,9 @@
+#1/usr/bin/env bash
+
+
+cd pair1
+touch scaffold.bash
+git add scaffold.bash
+git commit -m "Add scaffold.bash"
+git push origin master
+git status
